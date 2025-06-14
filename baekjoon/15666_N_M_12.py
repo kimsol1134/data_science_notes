ae9939b02fe20a9b33119d935f13ddb6f531fe89
kimsol1134 <kimsol1134@naver.com>
2025-06-14 23:11:27 +0900

    0614

---
+++ baekjoon/15666_N_M_12.py
@@ -0,0 +1,9 @@
+from itertools import combinations_with_replacement
+n, m = map(int, input().split())
+
+num_list = [int(x) for x in input().split()]
+num_list = sorted(num_list)
+result = list(combinations_with_replacement(num_list, m))
+result = sorted(list(set(result)))
+for w in result :
+    print(*w)
